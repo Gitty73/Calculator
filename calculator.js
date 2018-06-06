129525dc2fc34d544178f60b45307381e309acaa
Gops <gopichand7n3@gmail.com>
2018-06-06 21:08:09 +0530

    completed

---
+++ calculator.js
@@ -0,0 +1,37 @@
+var add = document.querySelector("#add");
+var sub = document.querySelector("#sub");
+var mul = document.querySelector("#mul");
+var div = document.querySelector("#div");
+var reset = document.querySelector("#reset");
+var total;
+var num1, num2, Result;
+
+function getValue(){
+	num1 = Number(document.querySelector("#n1").value);
+	num2 = Number(document.querySelector("#n2").value);
+	Result = document.querySelector("#result");
+};
+
+add.addEventListener("click", function(){
+	getValue();
+	total = num1 + num2;
+	Result.value = total;
+});
+
+sub.addEventListener("click", function(){
+	getValue();
+	total = num1 - num2;
+	Result.value = total;
+});
+
+mul.addEventListener("click", function(){
+	getValue();
+	total = num1 * num2;
+	Result.value = total;
+});
+
+div.addEventListener("click", function(){
+	getValue();
+	total = num1 / num2;
+	Result.value = total;
+});
